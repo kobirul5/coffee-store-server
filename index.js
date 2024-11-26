2afa8a783d9b7391b0e50597ad82250aa12115e7
kobirul5 <kobirul05j@gmail.com>
2024-11-26 22:00:25 +0600

    added app.post

--- index.js
+++ index.js
@@ -26,7 +26,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
+    const coffeeCollection = client.db("coffeeDB").collection("coffees")
+
+    app.post("/coffee", async(req, res)=>{
+      const newCoffee = req.body
+      const  result = await coffeeCollection.insertOne(newCoffee)
+      res.send(result)
+      // console.log(newCoffee)
+    })
 
 
 
