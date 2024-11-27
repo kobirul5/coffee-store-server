4c80ecdf4a8292d70404b49a89e249db6b6f7024
kobirul5 <kobirul05j@gmail.com>
2024-11-27 23:20:37 +0600

    fixed problem

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ async function run() {
 
     app.get("/coffee/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await coffeeCollection.findOne(query)
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeeCollection.findOne(query);
       res.send(result)
     })
 
